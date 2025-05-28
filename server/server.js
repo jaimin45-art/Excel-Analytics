8e1f17836fd2950a9d4ed36aa8388072c2cf0cf2
jaimin45-art <jmodi7396@gmail.com>
2025-05-28 10:29:07 +0530

    initial commit

---
+++ server/server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const cors = require('cors');
+const uploadRoutes = require('./routes/upload');
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.use('/api', uploadRoutes);
+
+app.listen(5000, () => {
+  console.log("Server running on http://localhost:5000");
+});
